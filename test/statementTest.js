77170d8488eb0195bb35d217fb0f8ee67b436229
penny <925580783@qq.com>
2020-08-27 18:54:47 +0800

    [refactorStatement][penny]feature:add htmlStatement test

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const {statement} = require('../src/statement');
+const {statement, HTMLStatement} = require('../src/statement');
 
 const plays = {
   'hamlet': {
@@ -180,4 +180,39 @@ test('Sample test7', t => {
   catch (e){
     t.is(e.message,expectResult);
   }
+});
+
+test('Sample test8', t => {
+  //given
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'hamlet',
+        'audience': 55,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35,
+      },
+      {
+        'playID': 'othello',
+        'audience': 40,
+      },
+    ],
+  };
+
+  const result = HTMLStatement(invoice, plays);
+
+  const expectResult = '<h1>Statement for BigCo</h1>\n' +
+      '<table>\n' +
+      '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+      ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+      ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+      ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+      '</table>\n' +
+      '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+      '<p>You earned <em>47</em> credits</p> \n';
+
+  t.is(result, expectResult);
 });
