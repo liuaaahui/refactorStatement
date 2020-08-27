b5c8b09cf89ef3c5c6224d4f7440806f5909c142
penny <925580783@qq.com>
2020-08-27 18:55:38 +0800

    [refactorStatement][penny]feature:implement generate htmlStatement

--- src/statement.js
+++ src/statement.js
@@ -78,10 +78,27 @@ function generateTxt(data) {
   return result;
 }
 
+function generateHTML(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.name}</td><td>${perf.audience}</td><td>${perf.thisAmount}</td></tr>\n`;
+  }
+  result += '</table>\n';
+  result += `<p>Amount owed is <em>${formatData(totalAmount(data.invoice, data.plays))}</em></p>\n`;
+  result += `<p>You earned <em>${volumeCredits(data.invoice, data.plays)}</em> credits</p> \n`;
+  return result;
+}
+
 function statement (invoice, plays) {
   return generateTxt(createData(invoice, plays));
 }
 
+function HTMLStatement (invoice, plays) {
+  return generateHTML(createData(invoice, plays));
+}
+
 module.exports = {
-  statement,
+  statement,HTMLStatement
 };
